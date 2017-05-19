5956745543fffc9e800a01e88e43e54172940458
Amir Abdullaev <acrofus@gmail.com>
2017-05-19 16:13:57 +0300

    Prettify

--- deanon.py
+++ deanon.py
@@ -3,4 +3,28 @@ from functools import reduce
 from json import load, dump
 
 with open('result.json', 'w') as result:
-    dump(list((lambda api, groups: set.intersection(*map((lambda g: set(reduce(lambda a, o: a + api.groups.getMembers(group_id=g, offset=o)['users'], range(0, api.groups.getMembers(group_id=g)['count'], 1000), []))), groups)))(API(Session()), load(open('groups.json')))), result)
+  dump(
+    list(
+      (
+        lambda api, groups: 
+          set.intersection(
+            *map(
+              lambda g: 
+                set(
+                  reduce(
+                    lambda a, o: 
+                      a + api.groups.getMembers(group_id=g, offset=o)['users'], 
+                    range(0, api.groups.getMembers(group_id=g)['count'], 1000), 
+                    []
+                  )
+                ), 
+              groups
+            )
+          )
+      )(
+        API(Session()), 
+        load(open('groups.json'))
+      )
+    ), 
+    result
+  )
